436e58acd1cb736928a865574e51db9a374cb57c
Jefferson <jalbuquerque39@gmail.com>
2024-09-24 12:12:01 -0300

    teste_aula

---
+++ Aula_03.js
@@ -0,0 +1,83 @@
+// estrutura if//
+/*
+if (condicao) {
+// bloco de código ue precisa ser executado
+}
+*/
+// primeiro exemplo de aula - estrutura padrão de if//
+/*let idadeLaura = 15;
+
+if (idadeLaura <= 18) {
+    console.log("Laura não pode tirar a habilitação");
+}
+*/    
+
+// exemplos condicionais//
+/*let numero = 12;
+
+if (numero == 12) {
+    console.log ("Eba, número 12");
+}
+
+if (numero == 18) {
+    console.log("Não é o número 12");
+}
+*/
+
+// if...else
+/*let corDeFundo = "vermelho"
+
+if (corDeFundo ==="azul") {
+    console.log("Usuario que está acessando é do Rio de janeiro");
+} else if (corDeFundo == "vermelho") { 
+    console.log("A cor de fundo vermelho, portanto o usuário que está acessando está em São Paulo");
+}
+*/
+
+/*let nomeDoUsuario = prompt("Digite o seu nome:");
+
+if (nomeDoUsuario == "") {
+    alert("usuário não preencheu seu nome");
+} else if (nomeDoUsuario == "Jefferson") {
+    alert("Bora estudar mas " + nomeDoUsuario + ", está precisando");
+} else {
+    alert("Será o Professor Júlio");
+}
+
+*/
+
+//variáveis booleanas//
+/*let idade = 37;
+let possui37anos = (idade == 37);
+console.log(possui37anos);
+if (possui37anos) {
+    alert("Essa pessoa possui 37 anos");
+}
+*/
+
+//para pensar//
+/*let valor;
+if (valor) {
+console.log("verdadeiro");
+} else {
+console.log("falso")
+}
+*/
+
+//estrimtamente igual
+/*let nome1 = "Júlio";
+let nome2 = "Jefferson";
+let nome3 = "Jefferson";*/
+
+/*if (nome1 === nome2) {
+    console.log("verdadeiro para valor e tipo");
+} else {
+    console.log("verdadeiro para tipo e falso para valor");
+}
+*/
+/*if (nome2 === nome3) {
+    console.log("verdadeiro para tipo e valor");
+} else {
+    console.log("verdadeiro ára tipo e falso para valor")
+}
+*/
